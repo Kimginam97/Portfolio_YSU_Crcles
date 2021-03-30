f0c890f8c551053ae720efb9fe1a9c170794b3af
Kimginam97 <rlarlska79@naver.com>
2021-03-30 20:35:12 +0900

    9.1.4.알림아이콘변경

---
+++ src/main/java/com/yeonsung/crcles/config/WebConfig.java
@@ -0,0 +1,31 @@
+package com.yeonsung.crcles.config;
+
+import com.yeonsung.crcles.notification.NotificationInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.StaticResourceLocation;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    private final NotificationInterceptor notificationInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        List<String> staticResourcesPath = Arrays.stream(StaticResourceLocation.values())
+                .flatMap(StaticResourceLocation::getPatterns)
+                .collect(Collectors.toList());
+        staticResourcesPath.add("/node_modules/**");
+
+        registry.addInterceptor(notificationInterceptor)
+                .excludePathPatterns(staticResourcesPath);
+    }
+}
+
--- src/main/java/com/yeonsung/crcles/notification/NotificationRepository.java
+++ src/main/java/com/yeonsung/crcles/notification/NotificationRepository.java
@@ -1,6 +1,8 @@
 package com.yeonsung.crcles.notification;
 
+import com.yeonsung.crcles.account.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NotificationRepository extends JpaRepository<Notification,Long> {
+    long countByAccountAndChecked(Account account, boolean checked);
 }
